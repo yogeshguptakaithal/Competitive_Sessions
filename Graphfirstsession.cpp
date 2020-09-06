9ee3e659c260af225465225cddeebfe81f7dace5
Yogesh Gupta <yogeshguptakaithal@gmail.com>
2020-09-06 11:10:47 +0530

    Create Graphfirstsession.cpp

---
+++ Graphfirstsession.cpp
@@ -0,0 +1,17 @@
+https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/tutorial/
+
+dfs->
+https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/tutorial/
+https://cp-algorithms.com/graph/depth-first-search.html
+
+Q-> https://codeforces.com/problemset/problem/580/C
+sol->https://codeforces.com/contest/580/submission/70590876
+
+Q->https://codeforces.com/problemset/problem/522/A
+sol->https://codeforces.com/contest/522/submission/71528930
+
+bfs->
+https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/tutorial/
+https://cp-algorithms.com/graph/breadth-first-search.html
+Q->https://practice.geeksforgeeks.org/problems/rotten-oranges/0
+sol->https://ideone.com/ke5JBW
